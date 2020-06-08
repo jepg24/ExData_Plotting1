5cf4d95d7d817ad8ce058f99cdc2cacb8cedc37f
Jesús Alberto Pérez Guerrero <62405968+jepg24@users.noreply.github.com>
2020-06-07 21:08:48 -0300

    Add files via upload

---
+++ Code.R
@@ -0,0 +1,54 @@
+## How much memory does it require to read the file
+## ~ memory required = n.col*n.row*8 bytes/numeric
+## ~ memory required = 9 * 2,075,259 * 8bytes = 149,418,648 ~ 0,15 G
+
+
+## Reading the Data
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, destfile = "data.zip", method = "curl")
+file <- unzip("data.zip", unzip = "internal")
+
+## Installing packages
+install.packages("data.table")
+library(data.table)
+
+## Subsetting the data
+df <- subset(Data,Data$Date == "1/2/2007" | Data$Date == "2/2/2007")
+Data <- data.table::fread("household_power_consumption.txt", stringsAsFactors = FALSE, dec = ".")
+datetime <- strptime(paste(df$Date, df$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+## Plot1
+## Getting the plot
+png("plot1.png", width = 480, height = 480, units = "px")
+hist(df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power(kilowatts)", col = "red")
+dev.off()
+
+## Plot2
+## Getting the plot
+png("plot2.png", width = 480, height = 480, units = "px")
+plot(datetime, df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+
+## Plot3
+png("plot3.png", width = 480, height = 480, units = "px")
+plot(datetime, df$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "", col = "black")
+lines(datetime, df$Sub_metering_2, col = "red")
+lines(datetime, df$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, c("Sub_meting_1", "Sub_meting_2", "Sub_meting_3"), col = c("black", "red", "blue"))
+dev.off()
+
+## Plot4
+png("plot4.png", width = 480, height = 480, units = "px")
+par(mfcol = c(2,2))
+## Plot1
+hist(as.numeric(df$Global_active_power), main = "", xlab = "Global Active Power(kilowatts)", col = "red")
+## Plot2
+plot(datetime, df$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "", col = "black")
+lines(datetime, df$Sub_metering_2, col = "red")
+lines(datetime, df$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, c("Sub_meting_1", "Sub_meting_2", "Sub_meting_3"), col = c("black", "red", "blue"))
+## Plot3
+plot(datetime, df$Voltage, type = "l", ylab = "Voltage", xlab = "")
+## Plot4
+plot(datetime, df$Global_reactive_power, type = "l", ylab = "Global Reactive Power", xlab = "")
+dev.off()
